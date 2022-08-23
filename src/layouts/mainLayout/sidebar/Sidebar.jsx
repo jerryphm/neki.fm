9a3eb86c0bd5c3beaf475ec20d8c6a0e80ce1ca8
phamthang11782 <phamthang11782@gmail.com>
2022-08-23 11:19:40 +0700

    responsive sidebar

--- src/layouts/mainLayout/sidebar/Sidebar.jsx
+++ src/layouts/mainLayout/sidebar/Sidebar.jsx
@@ -7,9 +7,9 @@ import client from '../../../client';
 import logo from '../../../assets/images/logo.png';
 import avatarFallback from '../../../assets/images/avatarFallback.jpg';
 import { RiHome5Line } from 'react-icons/ri';
-import { BiChart, BiAlbum, BiMenuAltRight } from 'react-icons/bi';
-import { BsMusicPlayer } from 'react-icons/bs';
+import { BiChart, BiAlbum } from 'react-icons/bi';
 import { FiSearch } from 'react-icons/fi';
+import { TbLayoutSidebarRightExpand } from 'react-icons/tb';
 import styled from 'styled-components';
 
 function Sidebar() {
@@ -17,7 +17,7 @@ function Sidebar() {
    const [userAvatar, setUserAvatar] = useState('');
    const { sk } = useSelector(authSelector);
    useEffect(() => {
-      (async () => {
+      const getUserInfo = async () => {
          const res = await client({
             url: `/?method=user.getInfo&sk=${sk}`,
          });
@@ -25,198 +25,165 @@ function Sidebar() {
          const userAvatar = res.data.user.image[3]['#text'] || avatarFallback;
          setUserName(userName);
          setUserAvatar(userAvatar);
-      })();
+      };
+      getUserInfo();
    }, []);
 
-   const toggleSideBar = () => {
-      const rawContainer = document.getElementsByTagName('aside')
-      const container = rawContainer['0']
-      
-      container.classList.toggle('collapse')
-   }
+   const navLinkArr = [
+      { to: '/', icon: <RiHome5Line />, display: 'Home' },
+      { to: '/search', icon: <FiSearch />, display: 'Search' },
+      { to: '/trend', icon: <BiChart />, display: 'Trend' },
+      { to: '/songs', icon: <BiAlbum />, display: 'Songs' },
+   ];
+   const toggleSideBarWidth = () => {
+      const rawContainer = document.getElementsByTagName('aside');
+      const container = rawContainer['0'];
+      container.classList.toggle('collapse');
+   };
    return (
-      <Container >
-         <OpenIcon onClick={toggleSideBar}>
-            <BiMenuAltRight />
-         </OpenIcon>
-         <Logo>
-            <img src={logo} alt='neki' />
+      <Container>
+         <TbLayoutSidebarRightExpand
+            className='sidebar__toggle-btn'
+            onClick={toggleSideBarWidth}
+         />
+         <div className='sidebar__logo'>
+            <img src={logo} />
             <span>
                <span>Neki</span>FM
             </span>
-         </Logo>
-         <Nav>
-            <NavLink
-               to='/'
-               className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-               <RiHome5Line />
-               <span>Home</span>
-            </NavLink>
-            <NavLink
-               to='search'
-               className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-               <FiSearch />
-               <span>Search</span>
-            </NavLink>
-            <NavLink
-               to='/trend'
-               className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-               <BiChart />
-               <span>Trend</span>
-            </NavLink>
-            <NavLink
-               to='/genres'
-               className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-               <BsMusicPlayer />
-               <span>Genres</span>
-            </NavLink>
-            <NavLink
-               to='/songs'
-               className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-               <BiAlbum />
-               <span>Songs</span>
-            </NavLink>
-         </Nav>
-         <User>
-            <img src={userAvatar} alt='' />
-            <p>{userName}</p>
-         </User>
+         </div>
+         <nav>
+            {navLinkArr.map((navLink, i) => (
+               <NavLink
+                  to={navLink.to}
+                  className={({ isActive }) => (isActive ? 'active' : '')}
+                  key={i}
+               >
+                  {navLink.icon}
+                  <span>{navLink.display}</span>
+               </NavLink>
+            ))}
+         </nav>
+         <div className='sidebar__user'>
+            <img src={userAvatar} />
+            <span>{userName}</span>
+         </div>
       </Container>
    );
 }
 
 export default Sidebar;
 const Container = styled.aside`
-   width: 250px;
-   padding: var(--padding-y) 18px var(--padding-y) var(--padding-x);
+   position: relative;
+   flex-shrink: 0;
+   flex-grow: 0;
+   width: clamp(200px, 19%, 235px);
+   padding: var(--padding-y) var(--padding-x);
    background-color: var(--white);
-   &.collapse {
-      width: 150px;
-   }
-`;
+   transition: 0.25s linear;
 
-const OpenIcon = styled.div`
-   display: flex;
-   justify-content: end;
-   align-items: center;
-   height: 40px;
-   text-align: right;
-   svg {
+   svg.sidebar__toggle-btn {
+      display: block;
+      height: 40px;
+      margin-left: auto;
       font-size: var(--font2xl);
-      color: var(--black);
+      color: var(--light-gray-text);
       cursor: pointer;
    }
-`;
-const Logo = styled.div`
-   display: flex;
-   align-items: end;
-   gap: 5px;
-   margin-top: 5px;
-   margin-bottom: 2.5rem;
-   img {
-      width: 35px;
-   }
-   span {
-      font-size: var(--fontlg);
+
+   .sidebar__logo {
+      display: flex;
+      align-items: end;
+      gap: 5px;
+      margin-top: 2.5rem;
+      margin-bottom: 2.5rem;
+      img {
+         width: 35px;
+      }
       span {
-         color: var(--red);
+         font-size: var(--fontlg);
+         span {
+            color: var(--red);
+         }
       }
    }
-`;
 
-const Nav = styled.nav`
-   display: flex;
-   flex-direction: column;
-   gap: 8px;
-   color: var(--gray-text);
-   & > span {
-      font-size: var(--fontsm);
-      color: var(--light-gray-text);
+   nav {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      a {
+         display: flex;
+         align-items: center;
+         gap: 1rem;
+         height: 4.4rem;
+         width: 100%;
+         padding-left: 1rem;
+         border-radius: 10px;
+         color: var(--gray-text);
+         svg {
+            font-size: var(--fontlg);
+         }
+      }
+      a.active {
+         position: relative;
+         background-color: var(--black);
+         color: var(--white);
+         &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: -24px;
+            bottom: 0;
+            width: 3px;
+            border-radius: 4px;
+            background-color: var(--black);
+            cursor: default;
+         }
+      }
    }
-   & > * {
+
+   .sidebar__user {
+      position: absolute;
+      bottom: 0px;
+      left: 0;
+      right: 0;
       display: flex;
       align-items: center;
-      gap: 1rem;
-      height: 44px;
-      width: 100%;
-      padding-left: 1rem;
-      border-radius: 10px;
-      svg {
-         font-size: var(--fontlg);
+      justify-content: center;
+      gap: 1.6rem;
+      height: 80px;
+      padding: 0 var(--padding-x);
+      border-top: 1px solid var(--light-gray-text);
+      user-select: none;
+      transition: .25s linear;
+      img {
+         width: 3rem;
+         height: 3rem;
+         border-radius: 5rem;
+      }
+      span {
+         white-space: nowrap;
+         overflow: hidden;
+         text-overflow: ellipsis;
       }
    }
-   & > *.active {
-      position: relative;
-      background-color: var(--black);
-      color: var(--white);
-      &::before {
-         content: '';
-         position: absolute;
-         top: 0;
-         right: -18px;
-         bottom: 0;
-         width: 3px;
-         border-radius: 4px;
-         background-color: var(--black);
-         cursor: default;
+
+   /* use for toggleSideBarWidth button */
+   span {
+      transition: 0.25s linear;
+   }
+   &.collapse {
+      width: 86px;
+      span {
+         opacity: 0;
+         font-size: 0px;
+      }
+      svg.sidebar__toggle-btn {
+         transform: rotate(180deg);
+      }
+      .sidebar__user {
+         gap: 0;  
       }
    }
 `;
-
-const User = styled.div`
-   position: absolute;
-   bottom: 0px;
-   left: 0;
-   right: 0;
-   display: flex;
-   align-items: center;
-   gap: 1.6rem;
-   height: 80px;
-   padding: 16px 34px;
-   border-top: 1px solid var(--light-gray-text);
-   user-select: none;
-   img {
-      width: 3rem;
-      height: 3rem;
-      border-radius: 5rem;
-   }
-   p {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-   }
-`;
-
-{
-   /* <span>Discover</span>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>New and Notable</span>
-            </Link>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>Release Calendar</span>
-            </Link>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>Events</span>
-            </Link>
-            <span>Collection</span>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>Favorite Songs</span>
-            </Link>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>Artist</span>
-            </Link>
-            <Link to='/'>
-               <AiOutlineHeart />
-               <span>Albums</span>
-            </Link> */
-}
